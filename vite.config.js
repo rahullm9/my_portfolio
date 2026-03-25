b641d0fcec7529341bd103d1ccd6d1cc607ab7b7
rahullm9 <rahulmahato10101@gmail.com>
2026-03-25 22:43:54 +0530

    increase chunk size

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     react()
   ],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 10000,
     rollupOptions: {
       output: {
         manualChunks(id) {
